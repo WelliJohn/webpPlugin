31755ffca912c44fe7d1094816eafd01ef7ea37c
WelliJohn <jiangweiwei@qccr.com>
2017-06-14 19:32:29 +0800

    add:Androidwebp插件编写完成

--- webppluginlibrary/src/main/groovy/org/wellijohn/androidwebp/AndroidWebpPlugin.groovy
+++ webppluginlibrary/src/main/groovy/org/wellijohn/androidwebp/AndroidWebpPlugin.groovy
@@ -1,67 +1,30 @@
 package org.wellijohn.androidwebp
 
-import org.gradle.api.*
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 class AndroidWebpPlugin implements Plugin<Project> {
-    void apply(Project project){
-        project.android.applicationVariants.all{//拿到的是ApplicationVariant
-            applicationVariant->applicationVariant.outputs.each{//拿到的是BaseVariantOutput
-                baseVariantOutput->
+    private static final String TASK_NAME = "WebpPlugin"
 
-                def webTask = project.task(type:WebpTask){
-                    resourcesPath = applicationVariant.mergeResources.outputDir
-                }
+    void apply(Project project) {
+        project.extensions.create("androidwebp", AndroidWebpConfig)
 
-                webTask.dependsOn variant.mergeResources
-                applicationVariant.processResources.dependsOn webTask
-
-            }
-        }
-    }
-
-
-    class WebpTask extends DefaultTask{
-        File resourcesPath;
-
-        @Override
-        Task doLast(Action<? super Task> action) {
-            Action action1 = new Action() {
-                @Override
-                void execute(Object o) {
-                    resourcesPath.eachDirMatch(~/^drawable.*|^mipmap.*/) { dir ->
-                        dir.eachFileMatch(FileType.FILES, ~/.*\.png$/) { file ->
-                            if(!(file.name ==~/.*\.9\.png$/)) {
-                                logger.debug("found file: ${file}")
-                                String webpFileName = file.getAbsolutePath()
-                                webpFileName = webpFileName[0..webpFileName.lastIndexOf('.')-1] + ".webp"
-                                logger.debug("webp file: ${webpFileName}")
-                                try {
-                                    def cwebp = [
-                                            "${project.androidwebp.cwebp}",
-                                            "-q",
-                                            "${project.androidwebp.quality}",
-                                            file,
-                                            "-o",
-                                            webpFileName
-                                    ]
-                                            .execute()
-                                    cwebp.waitFor()
-
-                                    if(cwebp.exitValue() != 0) {
-                                        logger.error("cwebp with error code ${cwebp.exitValue()} and: ${cwebp.err.text}")
-                                    } else {
-                                        file.delete()
-                                    }
-
-                                } catch(IOException ioe) {
-                                    logger.error("Could not find 'cwebp'. Tried these locations: ${System.getenv('PATH')}")
-                                }
-                            }
-                        }
+        project.android.applicationVariants.all {
+                //拿到的是ApplicationVariant
+            variant ->
+                variant.outputs.each { output ->
+                    def webpTask = project.task(type: WebpTask, "${TASK_NAME}${variant.name.capitalize()}") {
+                        resourcesPath = variant.mergeResources.outputDir
                     }
+
+                    webpTask.dependsOn variant.mergeResources
+                    output.processResources.dependsOn webpTask
                 }
-            }
-            return action1;
         }
     }
+}
+
+class AndroidWebpConfig{
+    String cwebp = "cwebp"
+    int quality = 80
 }
